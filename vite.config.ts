0e20d27d5ba60dfbdee6214380880134c8022861
Classmiss.in <249087317+270486ar@users.noreply.github.com>
2026-07-21 10:50:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/starac/',
+
   plugins: [react()],
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
